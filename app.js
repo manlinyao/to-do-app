5f0aaec2bb5bd8881e67d70786b7cee39b755b91
Manlin Yao <manlinyao18@gmail.com>
2020-01-07 17:09:06 -0600

    assignment 12 completed

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function onReady() {
   const addToDoForm = document.getElementById('addToDoForm');
-  const newToDoText = document.getElementById('newToDoText');
+  const newToDoText = document.getElementById('sample1');
   const toDoList = document.getElementById('toDoList');
 
   addToDoForm.addEventListener('submit', event => {
