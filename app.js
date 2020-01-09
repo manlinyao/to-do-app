85188f2fca7e17745fda7987e69c031943df7eb4
Manlin Yao <manlinyao18@gmail.com>
2020-01-08 19:53:18 -0600

    assignment 13 completed

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 function onReady() {
-  const toDos = [];
-  const addToDoForm = document.getElementById('addToDoForm')
+  let toDos = [];
+  const addToDoForm = document.getElementById('addToDoForm');
+  let id = 0;
 
   function createNewToDo() {
     const newToDoText = document.getElementById('newToDoText');
     if (!newToDoText.value) {return; }
+
     toDos.push({
       title: newToDoText.value,
-      complete: false
+      complete: false,
+      id: id
     });
-    newToDoText.value = '';
 
+    id++;
+
+    newToDoText.value = '';
     renderTheUI();
   }
 
@@ -24,10 +29,21 @@ function onReady() {
       const checkbox = document.createElement('input');
       checkbox.type = "checkbox";
 
+      const deleteBtn = document.createElement('button');
+      deleteBtn.textContent = "Delete!";
+
+      deleteBtn.addEventListener('click', event => {
+        toDos = toDos.filter(function(item) {
+          return item.id !== toDo.id;
+        })
+        renderTheUI();
+      });
+
       newLi.textContent = toDo.title;
 
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
+      newLi.appendChild(deleteBtn);
     });
   }
 
